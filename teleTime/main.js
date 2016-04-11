5f0f23825cd66e841e1f6ccb9d8a7863cd1447d8
Sujeethan <sujeethan.vishnumoorthy@gmail.com>
2016-04-11 16:26:46 -0400

    Chat is alive kinda
    Shitty but a chat

---
+++ teleTime/main.js
@@ -0,0 +1,33 @@
+Messages = new Mongo.Collection('messages');
+
+if(Meteor.isClient){
+	Template.messages.helpers({
+		messages: function(){
+			return Messages.find();
+		}
+	});
+	Template.messages.events({
+		'keypress textarea': function(e, instance){
+			if (e.keyCode == 13){
+				var value = instance.find('textarea').value;
+				instance.find('textarea').value = '';
+				
+				Messages.insert({
+					
+					message: value,  
+					timestamp: new Date()
+					//user: Meteor.userId
+					
+				});
+			}
+			
+		}
+		
+		
+	});
+}
+
+if(Meteor.isServer){
+	
+	
+}
