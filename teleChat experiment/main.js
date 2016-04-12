db6a4d7d1717cbe60db67fc7aa426c5da07fc0cc
Naveen Varna <naveenv@rogers.com>
2016-04-11 20:11:22 -0400

    Added files via upload

---
+++ teleChat experiment/main.js
@@ -0,0 +1,31 @@
+Messages = new Mongo.Collection('messages');
+Users = new Mongo.Collection('users');
+
+if(Meteor.isClient){
+	Template.messages.helpers({
+		messages: function(){
+			return Messages.find();
+		}
+	});
+	
+	Template.messages.events({
+		'keypress textarea': function(e, instance){
+			if (e.keyCode == 13){
+				var value = instance.find('textarea').value;
+				instance.find('textarea').value = '';
+				
+				Messages.insert({
+					message: value,  
+					timestamp: new Date()
+					//user: Meteor.userId		
+				});
+			}		
+		}
+		
+	});
+}
+
+if(Meteor.isServer){
+	
+	
+}
