4cd9f7d2b42bfc6ca072405faafa3dbd653c5e11
Naveen Varna <naveenv@rogers.com>
2016-04-13 13:32:18 -0400

    Partially working 3 chats
    Every chat is separate. Issue is the text in the chatbox when entered does not output anything
    Also eliminated useless css for the time being such as genres boxes which we dont need
    
    What's left so far: Implement ratings + online users + createFrom output + functional 3 chats + genres list overlaps the chats

--- teleChat/main.js
+++ teleChat/main.js
@@ -1,5 +1,6 @@
 Messages = new Mongo.Collection('messages');
-
+Messages = new Mongo.Collection('messages2');
+Messages = new Mongo.Collection('messages3');
 
 if(Meteor.isClient){
 	Template.messages.helpers({
@@ -8,6 +9,18 @@ if(Meteor.isClient){
 		}
 	});
 	
+	Template.messages2.helpers({
+		messages2: function(){
+			return Messages2.find();
+		}
+	});	
+	
+	Template.messages3.helpers({
+		messages3: function(){
+			return Messages3.find();
+		}
+	});	
+		
 	Template.messages.events({
 		'keypress textarea': function(e, instance){
 			if (e.keyCode == 13){
@@ -16,6 +29,38 @@ if(Meteor.isClient){
 				
 				Messages.insert({
 					message: value,  
+					timestamp: new Date(),
+					user: Meteor.userId()
+				});
+			}		
+		}
+		
+	});
+	
+	Template.messages2.events({
+		'keypress textarea': function(e, instance){
+			if (e.keyCode == 13){
+				var value = instance.find('textarea').value;
+				instance.find('textarea').value = '';
+				
+				Messages2.insert({
+					message2: value,  
+					timestamp: new Date(),
+					user: Meteor.userId()
+				});
+			}		
+		}
+		
+	});
+	
+		Template.messages3.events({
+		'keypress textarea': function(e, instance){
+			if (e.keyCode == 13){
+				var value = instance.find('textarea').value;
+				instance.find('textarea').value = '';
+				
+				Messages3.insert({
+					message3: value,  
 					timestamp: new Date(),
 					user: Meteor.userId()
 				});
@@ -33,6 +78,26 @@ if(Meteor.isClient){
 			return moment(this.timestamp).format('h:mm a');
 		}
 	});
+
+	Template.message2.helpers({
+		user: function() {
+			return Meteor.users.findOne({_id: this.user});
+		},
+		
+		time: function() {
+			return moment(this.timestamp).format('h:mm a');
+		}
+	});
+	
+	Template.message3.helpers({
+		user: function() {
+			return Meteor.users.findOne({_id: this.user});
+		},
+		
+		time: function() {
+			return moment(this.timestamp).format('h:mm a');
+		}
+	});
 	
 	Accounts.ui.config({
 		passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
@@ -40,7 +105,16 @@ if(Meteor.isClient){
 }
 
 if(Meteor.isServer){
-	
+	Meteor.publish('messages', function () {
+    return Messages.find({});
+});
+
+	Meteor.publish('messages2', function () {
+    return Messages2.find({});
+});
+
+	Meteor.publish('messages3', function () {
+    return Messages3.find({});
+});
 	
 }
-
